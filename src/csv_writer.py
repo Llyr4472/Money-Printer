aa583ca30645665b0bc24f0c4b3d671941f7e68c
llyr4472 <mr.prashant2077@gmail.com>
2024-05-05 18:34:18 +0545

    Add signals to file themselves

--- src/csv_writer.py
+++ src/csv_writer.py
@@ -8,9 +8,6 @@ import numpy as np
 
 
 def to_csv(stock):
-    if not isinstance(stock,Stock):stock = Stock(stock)
-    #data = stock.get_data()
-
     file_path = stock.file
 
     if(Path(file_path).exists()):
@@ -24,7 +21,7 @@ def to_csv(stock):
             
             # Write header row
             writer.writerow(["date", "high", "low", "close", "previous", "diff", "numtrans", "tradedshares", "volume"])
-            print(f"{stock.symbol}.csv created")
+            print(f"{stock.file} created")
         data = stock.get_data()
 
     # If no new data end process
@@ -36,18 +33,33 @@ def to_csv(stock):
         for date, info in data.items():
             price_info = info["price"]
             writer.writerow([date, price_info["max"], price_info["min"], price_info["close"], price_info["prevClose"], price_info["diff"], info["numTrans"], info["tradedShares"], info["amount"]])
-        print(f"{stock.symbol}.csv updated")
+        print(f"{stock.file} updated")
 
 def sort(stock):
-    if not isinstance(stock , Stock):stock = Stock(stock)
     df = pd.read_csv(stock.file)
     df_sorted = df.sort_values(by='date', ascending=True)
     if(not df.equals(df_sorted)):
-        df_sorted.to_csv(file_path, index=False)
-        print(f"{stock.symbol}.csv sorted")
+        df_sorted.to_csv(stock.file, index=False)
+        print(f"{stock.file} sorted")
+
+def add_signals(stock):
+    df = pd.read_csv(stock.file)
+    # MACD
+    df.ta.macd(close='close', fast=12, slow=26, signal=9, append=True)
+    #RSI
+    df['RSI'] = ta.rsi(df['close']) #append=True didn't work with RSI
+    #EMAS
+    emas_used = [3,5,8,10,12,15,30,35,40,45,50,60]
+    for ema in emas_used:
+        #df[f'ema{ema}'] = EMA(period=ema,input_values=df['close'].tolist())
+        df.ta.ema(length=ema,append=True).mean()
+
+    df.to_csv(stock.file, index=False)
 
 if __name__ == "__main__":
     for symbol in COMPANIES:
+        symbol = Stock(symbol)
         to_csv(symbol)
         sort(symbol)
+        add_signals(symbol)
     print("All csv dumped successfully")
