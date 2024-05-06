b049da709ca73ce9aa2b0370c7271bc811ca3daf
llyr4472 <mr.prashant2077@gmail.com>
2024-05-06 11:08:07 +0545

    Revert back to no signals in file

--- src/csv_writer.py
+++ src/csv_writer.py
@@ -7,18 +7,17 @@ import pandas_ta as ta
 import numpy as np
 
 
-def to_csv(stock):
-    file_path = stock.file
+def to_csv(stock,sorted=True):
 
-    if(Path(file_path).exists()):
-        df = pd.read_csv(file_path)
+    if(Path(stock.file).exists()):
+        df = pd.read_csv(stock.file)
         start_date = datetime.strptime(df.iloc[-1]['date'], '%Y-%m-%d') + timedelta(days=1)# Start from the day after the last date
         data = stock.get_data(start_date=start_date)
 
     else:
-        with open(file_path,'w',newline='') as f:
+        with open(stock.file,'w',newline='') as f:
             writer = csv.writer(f)
-            
+
             # Write header row
             writer.writerow(["date", "high", "low", "close", "previous", "diff", "numtrans", "tradedshares", "volume"])
             print(f"{stock.file} created")
@@ -28,12 +27,16 @@ def to_csv(stock):
     if not data: return
     
     #write data
-    with  open(file_path,'a',newline='') as f:
+    with  open(stock.file,'a',newline='') as f:
         writer = csv.writer(f)
         for date, info in data.items():
             price_info = info["price"]
             writer.writerow([date, price_info["max"], price_info["min"], price_info["close"], price_info["prevClose"], price_info["diff"], info["numTrans"], info["tradedShares"], info["amount"]])
         print(f"{stock.file} updated")
+
+    #sort
+    if sorted: sort(stock)
+
 
 def sort(stock):
     df = pd.read_csv(stock.file)
@@ -42,24 +45,7 @@ def sort(stock):
         df_sorted.to_csv(stock.file, index=False)
         print(f"{stock.file} sorted")
 
-def add_signals(stock):
-    df = pd.read_csv(stock.file)
-    # MACD
-    df.ta.macd(close='close', fast=12, slow=26, signal=9, append=True)
-    #RSI
-    df['RSI'] = ta.rsi(df['close']) #append=True didn't work with RSI
-    #EMAS
-    emas_used = [3,5,8,10,12,15,30,35,40,45,50,60]
-    for ema in emas_used:
-        #df[f'ema{ema}'] = EMA(period=ema,input_values=df['close'].tolist())
-        df.ta.ema(length=ema,append=True).mean()
-
-    df.to_csv(stock.file, index=False)
-
 if __name__ == "__main__":
-    for symbol in COMPANIES:
-        symbol = Stock(symbol)
+    for symbol in [Stock(x) for x in COMPANIES if Stock(x).trade] :
         to_csv(symbol)
-        sort(symbol)
-        add_signals(symbol)
     print("All csv dumped successfully")
