c3b36b00d980f40ff91fd84b804dad1ba493ddac
llyr4472 <mr.prashant2077@gmail.com>
2024-05-09 20:55:08 +0545

    update backtest.py

--- src/backtest.py
+++ src/backtest.py
@@ -1,35 +1,53 @@
 from Stock import *
 from signals import *
 import pandas as pd
+import plotter
 
 
-#stock = Stock(input("Stock: "))
-def backtest(stock,strategy='hybrid') :
+def backtest(stock,strategy='hybrid',plot=False) :
+    """
+    Performs a backtest of a given stock using the specified trading strategy.
+
+    Args:
+        stock (Stock): The stock object to backtest.
+        strategy (str, optional): The trading strategy to use for the backtest. Defaults to 'hybrid'.
+        plot (bool, optional): Whether to plot the results of the backtest. Defaults to False.
+
+    Returns:
+        None
+    """
+
     df = pd.read_csv(stock.file)
+    pd.to_datetime(df['date'])
 
     pos = 0
     num = 0
     percentchange = []
+    buy_dates = []
+    sell_dates = []
 
     for i in df.index:
         close = df['close'][i]
         date = df['date'][i]
 
+        df_trimmed = df.copy(deep=True)
+        df_trimmed= df_trimmed.loc[df_trimmed['date'] <= str(date)]
+
         #Check strategy and generate signal
         if strategy == 'hybrid':
-            s = Signal(stock=stock,date=date)
+            s = Signal(df_trimmed)
             signal =  'Buy' if   s>=0 else ('Sell' if s <=0 else 'Hold')
 
         elif strategy == 'ema_crossover':
-            s = ema_crossover(preprocess(stock=stock,date=date))
+            s = ema_crossover(df_trimmed)
             signal = 'Buy' if s>0 else ('Sell' if s <0 else 'Hold')
 
         elif  strategy == 'macd':
-            s = macd(preprocess(stock=stock,date=date))
+            s = macd(df_trimmed)
             signal = 'Buy' if s>0 else ('Sell' if s <0 else 'Hold')
 
         elif strategy  == 'rsi':
-            s = rsi(preprocess(stock=stock,date=date))
+            s = rsi(df_trimmed)
             signal = 'Buy' if s>0 else ('Sell' if s <0 else 'Hold')
 
         else:
@@ -39,12 +57,14 @@ def backtest(stock,strategy='hybrid') :
         if signal=='Buy' and pos==0:
             bp=close
             pos=1
+            buy_dates.append(date)
         
         elif signal=='Sell'  and pos==1:
             pos=0
             sp=close
             pc=(sp/bp-1)*100
             percentchange.append(pc)
+            sell_dates.append(date)
     
     #If its last day, sell the units
         if(num==df["close"].count()-1 and pos==1):
@@ -98,7 +118,7 @@ def backtest(stock,strategy='hybrid') :
 
     #print results
     print()
-    print("Results for "+ stock() +" going back to "+str(df.index[0])+", Sample size: "+str(ng+nl)+" trades"+" using " + strategy + " strategy.")
+    print("Results for "+ stock() +" going back to "+str(df.date[0])+", Sample size: "+str(ng+nl)+" trades"+" using " + strategy + " strategy.")
     print("Batting Avg: "+ str(battingAvg))
     print("Gain/loss ratio: "+ ratio)
     print("Average Gain: "+ str(avgGain))
@@ -108,6 +128,10 @@ def backtest(stock,strategy='hybrid') :
     print("Total return over "+str(ng+nl)+ " trades: "+ str(totalR)+"%")
     print()
 
+    #plot results
+    if plot:
+        plotter.plot(df,buy_dates,sell_dates)
+
 if __name__ == '__main__':
-    stock = Stock('akjcl')
-    backtest(stock,strategy='hybrid')
+    stock = Stock('uhewa')
+    backtest(stock,strategy='macd',plot=True)
